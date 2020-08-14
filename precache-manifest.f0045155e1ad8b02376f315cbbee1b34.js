b820eca1524ee4390fa255b79d10d6976cf11ec2
Shannon Makenna <shannonkariuki@gmail.com>
2020-08-13 23:02:53 -0400

    Updates

---
+++ precache-manifest.f0045155e1ad8b02376f315cbbee1b34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6a15305c6317e8d8826b30c2ee69c8",
+    "url": "/ExpenseTracker/index.html"
+  },
+  {
+    "revision": "ff57a87207f068e69d77",
+    "url": "/ExpenseTracker/static/css/main.780db706.chunk.css"
+  },
+  {
+    "revision": "cc6d4e6b966c5472fabd",
+    "url": "/ExpenseTracker/static/js/2.a07abce3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ExpenseTracker/static/js/2.a07abce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff57a87207f068e69d77",
+    "url": "/ExpenseTracker/static/js/main.b897b175.chunk.js"
+  },
+  {
+    "revision": "5f7f09647a4f8d2c1d94",
+    "url": "/ExpenseTracker/static/js/runtime-main.09c03126.js"
+  }
+]);
